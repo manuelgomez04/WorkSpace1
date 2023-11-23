14471e7b9a4f8d17c49ca0fa61fca0e578bb5f46
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2023-11-23 13:30:38 +0100

    Cambios en ejercicio 10

--- EjerciciosUD3Parte1ManuelGomez/src/ejercicio10/Maquina.java
+++ EjerciciosUD3Parte1ManuelGomez/src/ejercicio10/Maquina.java
@@ -1,28 +1,2 @@
 package ejercicio10;
 
-import java.util.Arrays;
-
-public class Maquina {
-
-	private Ticket billete;
-	private 
-
-	
-	public void setBillete(Ticket billete) {
-		this.billete = billete;
-	}
-
-	public Ticket getBillete() {
-		return billete;
-	}
-
-	public double calcularPrecioBillete (int cantidad) {
-		return precioUnitario*cantidad;
-	}
-	
-	public void mostrarCantidad (int cantidad) {
-		System.out.println(calcularPrecioBillete(cantidad));
-	}
-
-}
-
