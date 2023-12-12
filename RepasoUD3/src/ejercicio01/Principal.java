1c1df19e0c57b80941b593dd9b125685d384ef99
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2023-12-12 13:32:15 +0100

    Repaso métodos y cosas día uno

---
+++ RepasoUD3/src/ejercicio01/Principal.java
@@ -0,0 +1,19 @@
+package ejercicio01;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		
+		Producto p = new Producto ("Lechuga", 1.20, 1, true);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ RepasoUD3/src/ejercicio01/Producto.java
@@ -0,0 +1,23 @@
+package ejercicio01;
+
+public class Producto {
+
+	private String nombre;
+	private double pFabrica;
+	private int seccion;
+	private boolean enVenta;
+	
+	public Producto(String nombre, double pFabrica, int seccion, boolean enVenta) {
+		
+		this.nombre = nombre;
+		this.pFabrica = pFabrica;
+		this.seccion = seccion;
+		this.enVenta = enVenta;
+	}
+	
+	
+	
+	
+	
+	
+}
