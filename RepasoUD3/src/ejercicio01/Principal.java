55a6e4e3cdf4654f81d1eff7cd5e0b1e8336e164
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2023-12-18 12:29:22 +0100

    Nooooooooooooooooooooooooooove la q he liao churrita que pasada

--- RepasoUD3/src/ejercicio01/Principal.java
+++ RepasoUD3/src/ejercicio01/Principal.java
@@ -2,113 +2,131 @@ package ejercicio01;
 
 import java.util.Scanner;
 
-
 public class Principal {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
+		Scanner sc = new Scanner(System.in);
 		String aux, nombre;
-		double pFabrica;
+		double pFabrica, porcentaje, porcGanan;
 		int seccion;
 		boolean activo;
-		int activoo, opcion, opcion2, tam = 15,  id = 1;
+		int activoo, opcion, pos, tam = 15, id = 1, numProduct = 0, numTrab = 5;
 		Tienda t;
-		Producto p; 
-		Producto lista [] = new Producto [tam];
-		
-		
-		
+
+		Producto p;
+		Producto lista[] = new Producto[tam];
+		t = new Tienda(lista, numProduct, numTrab);
+
 		do {
 			System.out.println("""
 					0 ---> Salir
 					1 ---> Rellenar productos
-					
+					2 ---> Mostrar productos
+					3 ---> Mostrar por sección
+					4 ---> Calcular PVP
+					5 ---> Cambiar precio de una categoría
+					6 ---> Calcular descuento
+					7 ---> "Eliminar" una seccion
+					8 ---> Mostrar solo activos
 					""");
-			aux  = sc.nextLine();
+			aux = sc.nextLine();
 			opcion = Integer.parseInt(aux);
-			
+
 			switch (opcion) {
 			case 1:
-				do {
-					
-				
-				System.out.println("Diga nombre");
+				System.out.println("Diga el nombre del producto");
 				nombre = sc.nextLine();
-				
-				System.out.println("Diga precio de fábrica");
+
+				System.out.println("Diga el precio de fábrica");
 				aux = sc.nextLine();
 				pFabrica = Double.parseDouble(aux);
-				
-				System.out.println("Diga sección (1 comida, 2 electronica 3 día a día");
+
+				System.out.println("Diga seccion");
 				aux = sc.nextLine();
 				seccion = Integer.parseInt(aux);
-				
-				System.out.println("¿Está activo? 1 para sí, 2 para no");
+
+				System.out.println("Diga si está en venta 1 para sí, 0 para no");
 				aux = sc.nextLine();
 				activoo = Integer.parseInt(aux);
 				if (activoo == 1) {
 					activo = true;
-				}else {
+				} else {
 					activo = false;
 				}
 
+				p = new Producto(nombre, pFabrica, seccion, activo, id);
 				id++;
-				
-				t = new Tienda(lista, 5);
-				t.agregar(new Producto (nombre, pFabrica, seccion, activo, id));
-				
-				System.out.println("0 para salir, cualquier número para seguir");
-				aux = sc.nextLine();
-				opcion = Integer.parseInt(aux);
-				
-				} while (opcion != 0 && t.getNumProduct()< lista.length );
-				
-				do {
-					
-				
-				
-					System.out.println("""
-							1 ---> Mostrar lista
-							2 ---> Buscar por seccion
-							""");
-					aux = sc.nextLine();
-					opcion2 = Integer.parseInt(aux);
-					
-					switch (opcion2) {
-					
-					
-						case 1:
-							t.mostrar();
-						break;
-						
-						case 2: 
-							System.out.println("Diga seccion");
-							aux = sc.nextLine();
-							seccion = Integer.parseInt(aux);
-							
-							t.mostarListaNoNull(t.buscarBySeccion(seccion));
-						 
-						default:
-						break;
-					}
-				
-				} while (opcion2 != 0);
+
+				t.agregar(p);
 				break;
-				
-			case 0 :
-				
-				System.out.println("Gracias por usar el programa");
-				
-			break;
-			
-			default:
+
+			case 2:
+				t.mostarListaNoNull(lista);
+				break;
+			case 3:
+				System.out.println("Diga la sección que desea ver");
+				aux = sc.nextLine();
+				seccion = Integer.parseInt(aux);
+
+				t.mostarListaNoNull(t.buscarBySeccion(seccion));
+				break;
+
+			case 4:
+				System.out.println("Diga la id del producto");
+				aux = sc.nextLine();
+				pos = Integer.parseInt(aux);
+
+				System.out.println("Diga el porcentaje de ganancias que deseas tener");
+				aux = sc.nextLine();
+				porcGanan = Double.parseDouble(aux);
+
+				t.PVP(pos, porcGanan);
+			case 5:
+				System.out.println("Diga sección");
+				aux = sc.nextLine();
+				seccion = Integer.parseInt(aux);
+
+				System.out.println("Diga nuevo precio");
+				aux = sc.nextLine();
+				pFabrica = Double.parseDouble(aux);
+
+				t.modificarPrecioSeccion(seccion, pFabrica);
+				break;
+
+			case 6:
+
+				System.out.println("Diga el descuento que desea");
+				aux = sc.nextLine();
+				porcentaje = Double.parseDouble(aux);
+
+				System.out.println("Diga el porcentaje de ganancias");
+				aux = sc.nextLine();
+				porcGanan = Double.parseDouble(aux);
+
+				System.out.println("Diga la id del producto");
+				aux = sc.nextLine();
+				pos = Integer.parseInt(aux);
+
+				t.hacerDescuento(porcentaje, id, porcGanan);
+
+				break;
+
+			case 7:
+				System.out.println("Diga sección");
+				aux = sc.nextLine();
+				seccion = Integer.parseInt(aux);
+
+				t.categoriaInactiva(seccion);
+				break;
+
+			case 8:
+				t.mostrarActivos();
 				break;
 			}
-			
-			
+
+			;
 		} while (opcion != 0);
-	
-		
+
 	}
 
 }
--- RepasoUD3/src/ejercicio01/Tienda.java
+++ RepasoUD3/src/ejercicio01/Tienda.java
@@ -4,14 +4,21 @@ import java.util.Arrays;
 
 public class Tienda {
 
-	private Producto lista [];
+	private Producto lista[];
 	private int numProduct;
 	private int numTrab;
-	
-	public Tienda(Producto[] lista, int numTrab) {
+
+	public Tienda(Producto[] lista, int numProduct, int numTrab) {
+		super();
 		this.lista = lista;
+		this.numProduct = numProduct;
 		this.numTrab = numTrab;
 	}
+
+	// VERVO EN INFINITIVO, SIN Ñ NI TILDE, QUE INDIQUE QUE HACE EL MÉTODO, LA
+	// SEGUNDA PALABRA EMPIEZA EN MAYÚSCULA Y LA PRIMERA EN MINÚSCULA
+	// SIEMPRE, EN SERIO, SIEMPRE, QUERIDO YO DEL FUTURO S I E M P R E ( A L W A Y S
+	// )
 
 	public Producto[] getLista() {
 		return lista;
@@ -37,28 +44,24 @@ public class Tienda {
 		this.numTrab = numTrab;
 	}
 
-	
 	public String toString() {
 		return "Tienda [lista=" + Arrays.toString(lista) + ", numProduct=" + numProduct + ", numTrab=" + numTrab + "]";
 	}
-	
-	// VERVO EN INFINITIVO, SIN Ñ NI TILDE, QUE INDIQUE QUE HACE EL MÉTODO, LA SEGUNDA PALABRA EMPIEZA EN MAYÚSCULA Y LA PRIMERA EN MINÚSCULA
-	// SIEMPRE, EN SERIO, SIEMPRE, QUERIDO YO DEL FUTURO    S I E M P R E (  A L W A Y S  )
-	
+
 	public void agregar(Producto p) {
-		lista [numProduct++] = p; 	
+		lista[numProduct++] = p;
 	}
-	
-	public void mostrar () {
+
+	public void mostrar() {
 		for (int i = 0; i < numProduct; i++) {
 			System.out.println(lista[i]);
 		}
 	}
-	
-	public int buscarId (int id) {
+
+	public Producto buscarId(int id) {
 		boolean encontrado = false;
 		int i = 0;
-		
+
 		while (!encontrado && i < numProduct) {
 			if (lista[i].getId() == id) {
 				encontrado = true;
@@ -66,44 +69,100 @@ public class Tienda {
 				i++;
 			}
 		}
-		
+
 		if (encontrado) {
-			return i;
+			return lista[i];
 		} else {
-			return -1;
+			return null;
 		}
 	}
-	
-	/*Buscar por sección*/
-	
-	public Producto [] buscarBySeccion (int categoria) {
+
+	/* Buscar por sección */
+
+	public Producto[] buscarBySeccion(int seccion) {
 		int i = 0;
-		Producto listaEncontrados [] = new Producto [numProduct];
-		
-		while ( i < numProduct) {
-			if (lista[i].getSeccion() == categoria) {
-				
-				listaEncontrados[i] = lista [i];
-				
-			} 
-			i++; 
-		}
-		
-			return listaEncontrados;
-		
-	}
-	
-	/*ESTE MÉTODO MUESTRA CUALQUIER LISTA DE PRODUCTO*/
-	public void mostarListaNoNull (Producto pLista []) {
-		for (int i = 0; i < pLista.length; i++) {
-			if (lista[i] != null) {
-			System.out.println(pLista[i]);
+		Producto listaEncontrados[] = new Producto[numProduct];
+
+		while (i < numProduct) {
+			if (lista[i].getSeccion() == seccion) {
+
+				listaEncontrados[i] = lista[i];
+
 			}
+			i++;
+		}
+
+		return listaEncontrados;
+
+	}
+
+	/* ESTE MÉTODO MUESTRA CUALQUIER LISTA DE PRODUCTO */
+
+	public void mostarListaNoNull(Producto pLista[]) {
+		int i = 0;
+
+		while (i < pLista.length) {
+			if (pLista[i] != null) {
+				System.out.println(pLista[i]);
+			}
+			i++;
+		}
+
+	}
+
+	/* Modificar precio fabrica a todos los productos por sección */
+
+	public void modificarPrecioSeccion(int seccion, double nuevoPrecio) {
+		int i = 0;
+
+		while (i < numProduct) {
+			if (seccion == lista[i].getSeccion()) {
+				lista[i].setpFabrica(nuevoPrecio);
+			}
+			i++;
+
+		}
+
+	}
+
+	public double PVP(int id, double porcentaje) {
+		Producto pr;
+		pr = buscarId(id);
+		double cien = 100;
+
+		return pr.getpFabrica() + (pr.getpFabrica() * porcentaje / cien);
+
+	}
+
+	public double hacerDescuento(double descuento, int id, double ganancia) {
+
+		double cien = 100;
+
+		return PVP(id, ganancia) - PVP(id, ganancia) * descuento / cien;
+	}
+
+	// DEVUELVE EL NUM DE ELEMENTOS ELIMINADOS
+	public int categoriaInactiva(int seccion) {
+		int i = 0;
+
+		while (i < numProduct) {
+			if (seccion == lista[i].getSeccion()) {
+				lista[i].setEnVenta(false);
+				i++;
+			}
+
+		}
+		return i;
+	}
+
+	public void mostrarActivos() {
+		int i = 0;
+
+		while (i < numProduct) {
+			if (lista[i].isEnVenta()) {
+				System.out.println(lista[i]);
+			}
+			i++;
 		}
 	}
-	
-	
-	/*public Producto buscarMasBarato () {
-		
-	}*/
 }
